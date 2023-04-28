d401438b3f3ad7b39cb7d9b17a445ad81e36e97a
Enias Cailliau <enias@steamship.com>
2023-04-27 20:27:42 -0700

    Add back inputs

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ from steamship import Steamship
 from my_agent import MyAgent
 from src.utils import show_result
 from tools import MyTool
+from termcolor import colored
 
 if __name__ == "__main__":
     with Steamship.temporary_workspace() as client:
         agent = MyAgent(client)
         if agent.is_ready():
-            results = agent.run("Generate a picture of boston")
+            message = input(colored("Input: ", "grey"))
+            results = agent.run(message)
             for result in results:
                 show_result(client, result)
         else:
             my_tool = MyTool(client)
-            result = my_tool.run("Find a hotel in Boston")
+            message = input(colored("Input: ", "grey"))
+            result = my_tool.run(message)
             print(result)
