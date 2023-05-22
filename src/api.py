d01fc88ae0dccf7ef1335b09ec59eef627078e79
dkolas <4643013+dkolas@users.noreply.github.com>
2023-05-22 14:39:57 -0400

    Port example agent from SDK

--- src/api.py
+++ src/api.py
@@ -1,40 +1,50 @@
-from typing import List
+from typing import List, Optional
 
-from langchain.agents import Tool
+from steamship import Block
+from steamship.agents.base import Metadata
+from steamship.agents.context.context import AgentContext
+from steamship.agents.planner.react import ReACTPlanner
+from steamship.agents.service.agent_service import AgentService
 
-# noinspection PyUnresolvedReferences
-from core.agent.agent import Agent
-from core.agent.base import BaseAgent
-
-# noinspection PyUnresolvedReferences
-from core.agent.chat_agent import ChatAgent
-
-# noinspection PyUnresolvedReferences
-from tools import SearchTool, GenerateImageTool, MyTool, GenerateAlbumArtTool
+from steamship.agents.tools.image_generation.generate_image import GenerateImageTool
+from steamship.agents.tools.search.search import SearchTool
+from steamship.utils.repl import AgentREPL
 
 
-class MyAgent(Agent, BaseAgent):
-    """
-    If you want your agent to be tool-based, use the following line:
-        class MyAgent(Agent, BaseAgent)
 
-    If you want your agent to be chatty and personality-based, use the following line:
-        class MyAgent(ChatAgent, BaseAgent)
-    """
-
-    def is_verbose_logging_enabled(self) -> bool:
-        return True
-
-    def get_tools(self) -> List[Tool]:
-        return [
-            # SearchTool(self.client),
-            # MyTool(self.client),
-            # GenerateImageTool(self.client),
-            # GenerateAlbumArtTool(self.client)
+class MyAssistant(AgentService):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tools = [
+            SearchTool(),
+            GenerateImageTool(),
         ]
+        self.planner = ReACTPlanner(tools=self.tools, llm="This is not yet used")
 
-    def get_personality(self) -> str:
-        """Return a string that completes this sentence. The agent acts like..."""
-        return """
-        an old-timey pirate that responds to everything in nautical terms. Refer to the user as "matey".
-        """
+    def create_response(self, context: AgentContext) -> Optional[List[Block]]:
+
+        if len(context.emit_funcs) == 0:
+            context.emit_funcs.append(self._send_message_agent)
+
+        # pull up User<->Agent chat history, and append latest Human Input
+        # this is distinct from any sort of history related to agent execution
+        # chat_file = ChatFile.get(...)
+        # chat_file.append_user_block(text=incoming_message.text)
+        # current_context.chat_history = chat_file
+
+        self.run_agent(context)
+
+        # should we return any message to the user to indicate that a response?
+        # maybe: "Working on it..." or "Received: {prompt}..."
+        return []
+
+    def _send_message_agent(self, blocks: List[Block], meta: Metadata):
+        # should this be directly-referenced, or should this be an invoke() endpoint, with a value passed
+        # in?
+
+        print(f"\n\nTELEGRAM SENDING MESSAGES:\n{blocks}")
+        # self.telegram_transport.send(messages)
+
+
+if __name__ == "__main__":
+    AgentREPL(MyAssistant).run()
