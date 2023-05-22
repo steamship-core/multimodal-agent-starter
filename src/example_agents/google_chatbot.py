f8a2386e097623606ab8f48c41d35dad16fd88c1
Ted Benson <ted@steamship.com>
2023-05-22 16:39:45 -0400

    YEAH

---
+++ src/example_agents/google_chatbot.py
@@ -0,0 +1,54 @@
+from typing import List, Optional
+
+from steamship import Block
+from steamship.agents.base import Metadata
+from steamship.agents.context.context import AgentContext
+from steamship.agents.planner.react import ReACTPlanner
+from steamship.agents.service.agent_service import AgentService
+
+from steamship.agents.tools.image_generation.generate_image import GenerateImageTool
+from steamship.agents.tools.search.search import SearchTool
+from steamship.utils.repl import AgentREPL
+from prompts import PROMPT
+
+
+class GoogleChatbot(AgentService):
+    """Deployable Multimodal Agent that lets you talk to Google Search & Google Images.
+
+    NOTE: To extend and deploy this agent, copy and paste the code into api.py.
+
+
+    """
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.tools = [
+            SearchTool(),
+            GoogleImagesSearch(),
+        ]
+        self.planner = ReACTPlanner(tools=self.tools, llm="This is not yet used")
+        self.planner.PROMPT = PROMPT
+
+    def create_response(self, context: AgentContext) -> Optional[List[Block]]:
+
+        if len(context.emit_funcs) == 0:
+            context.emit_funcs.append(self._send_message_agent)
+
+        if len(context.chat_history.messages) == 0:
+            context.chat_history.append_system_message()
+
+        self.run_agent(context)
+
+        # should we return any message to the user to indicate that a response?
+        # maybe: "Working on it..." or "Received: {prompt}..."
+        return []
+
+    def _send_message_agent(self, blocks: List[Block], meta: Metadata):
+        # should this be directly-referenced, or should this be an invoke() endpoint, with a value passed
+        # in?
+
+        print(f"\n\nTELEGRAM SENDING MESSAGES:\n{blocks}")
+        # self.telegram_transport.send(messages)
+
+
+if __name__ == "__main__":
+    AgentREPL(GoogleChatbot).run()
